81ed9ccc66afbff4dfbe9fdf1500711020cf2be1
ta-mu-aa <tamtamtamtaaaf@gmail.com>
2022-07-24 23:58:51 +0900

    新規登録画面の大枠を作成

---
+++ front/src/components/pages/SignUpPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex justify-center h-full py-14">
+    <div class="container lg: py-14">
+      <div>
+        新規登録ページ
+        <div>名前<input type="text"></div>
+        <div>メールアドレス<input type="text"></div>
+        <div>パスワード<input type="text"></div>
+        <div>パスワード（確認用）<input type="text"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
