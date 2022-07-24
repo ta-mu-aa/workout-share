70efbae7411899b5509f9b3dabb80c7921003a5a
ta-mu-aa <tamtamtamtaaaf@gmail.com>
2022-07-24 23:56:32 +0900

    ログイン後ページの大枠を作成

---
+++ front/src/components/pages/HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex justify-center h-full py-14">
+    <div class="container lg:">
+      ログイン後ページ
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
