0da15e20a26a16d8590d6f05aa7d871fae889865
ta-mu-aa <tamtamtamtaaaf@gmail.com>
2022-08-02 21:37:21 +0900

    Userコントローラーを作成しJSONでレスポンスが返せるところまで

---
+++ api/app/controllers/user_controller.rb
@@ -0,0 +1,15 @@
+class UserController < ApplicationController
+  def index
+    render json:{data: "data"}, status:200
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+end
