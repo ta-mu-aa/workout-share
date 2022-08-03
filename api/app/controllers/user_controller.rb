74145e1459ef1493bd323018b2405da4e4bb97f9
ta-mu-aa <tamtamtamtaaaf@gmail.com>
2022-08-03 21:47:12 +0900

    新規ユーザーを作成するメソッドを実装

--- api/app/controllers/user_controller.rb
+++ api/app/controllers/user_controller.rb
@@ -4,6 +4,16 @@ class UserController < ApplicationController
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      render json: { id:user.id, name:user.name, email:user.email }, status:201
+    else
+      if user.errors[:email] === ["has already been taken"]
+        render json: { message: '既に登録されています', data: user.errors[:email] }, status: 409
+      else
+        render json: { message: '正しく入力してください', data: user.errors}, status: 400
+      end
+    end
   end
 
   def update
@@ -12,4 +22,8 @@ class UserController < ApplicationController
   def delete
   end
 
+  private 
+  def user_params
+    params.permit(:name, :email, :password, :password_confirmation)
+  end
 end
