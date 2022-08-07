69b043bcf6e508ac9165e738bf7b8d0986607d66
ta-mu-aa <tamtamtamtaaaf@gmail.com>
2022-08-07 21:58:52 +0900

    API側でcorsの設定を追加しフロント側に環境変数でURLを指定

--- front/src/components/pages/SignUpPage.vue
+++ front/src/components/pages/SignUpPage.vue
@@ -7,13 +7,18 @@
         <UserFormPassword />
         <UserFormConfirm />
         <div class="md:flex md:items-center mb-8">
-        <div class="md:w-1/3"></div>
-          <span class="text-xs"><router-link to="/login" class="text-blue-700 hover:text-blue-500 duration-200 cursor-pointer">ログイン</router-link>はこちらから</span>
+          <div class="md:w-1/3"></div>
+          <span class="text-xs">
+            <router-link to="/login" class="text-blue-700 hover:text-blue-500 duration-200 cursor-pointer">ログイン
+            </router-link>はこちらから
+          </span>
         </div>
         <div class="md:flex md:items-center">
           <div class="md:w-1/3"></div>
           <div class="md:w-2/3">
-            <button class="shadow bg-blue-500 hover:bg-blue-400 duration-200 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+            <button @click="registerDataSubmit"
+              class="shadow bg-blue-500 hover:bg-blue-400 duration-200 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+              type="button">
               新規会員登録
             </button>
           </div>
@@ -28,12 +33,28 @@ import UserFormName from '../UsreForm/UserFormName.vue'
 import UserFormEmail from '../UsreForm/UserFormEmail.vue'
 import UserFormPassword from '../UsreForm/UserFormPassword.vue'
 import UserFormConfirm from '../UsreForm/UserFormConfirm.vue'
+const API_URL = import.meta.env.VITE_API_URL
 export default {
   components: {
     UserFormName,
     UserFormEmail,
     UserFormPassword,
     UserFormConfirm
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+    registerDataSubmit() {
+      this.axios.get(`${API_URL}/user/`)
+        .then((response) => {
+          console.log(response)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
   }
 }
 </script>
