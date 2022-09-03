a371df41128b71aa6fbd72790600542f669e8814
ta-mu-aa <tamtamtamtaaaf@gmail.com>
2022-09-03 13:37:53 +0900

    Vuexをインストールし使用できるようにファイルを設定

--- front/src/main.js
+++ front/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import VueRouter from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import { store } from '../store/index.js' 
 import './assets/styles/main.scss'
 import './assets/styles/reset.scss'
 import './assets/styles/tailwind.scss'
@@ -13,4 +14,5 @@ axios.defaults.withCredentials = true
 const app = createApp(App)
   app.use(VueRouter)
   app.use(VueAxios, axios)
+  app.use(store)
   app.mount('#app')
---
+++ front/store/index.js
@@ -0,0 +1,18 @@
+import { createStore } from 'vuex' 
+
+export const store = createStore({
+  state() { 
+    return {
+      data:'vuex'
+    }
+  },
+  mutations: {
+    
+  },
+  actions: {
+
+  },
+  getters: {
+    
+  }
+})
