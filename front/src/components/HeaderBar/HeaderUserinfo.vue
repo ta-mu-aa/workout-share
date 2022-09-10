603c11342042d8e46e83b28126e86fcacf120e4f
ta-mu-aa <tamtamtamtaaaf@gmail.com>
2022-09-10 17:53:33 +0900

    ユーザ情報編集用ボタンとログアウトボタンを表示するドロワーを作成

--- front/src/components/HeaderBar/HeaderUserinfo.vue
+++ front/src/components/HeaderBar/HeaderUserinfo.vue
@@ -1,20 +1,51 @@
 <template>
-<a href="#" class="flex-shrink-0 group block">
-  <div class="flex items-center group-hover:opacity-60 duration-200">
-    <div>
-      <img class="inline-block  md:h-10 md:w-10 h-9 w-9  rounded-full" src="https://pbs.twimg.com/profile_images/1121328878142853120/e-rpjoJi_bigger.png" alt="" />
-    </div>
-    <div class="ml-3 hidden md:block">
-      <p class="text-sm leading-6 font-medium">
-        筋トレ 太郎
-      </p>
+  <div ref="wrapper" class="relative settingDrawer_wrapper">
+    <a href="#" class="flex-shrink-0 group block" @click="showDrower">
+      <div class="flex items-center group-hover:opacity-60 duration-200">
+        <div>
+          <img class="inline-block  md:h-10 md:w-10 h-9 w-9  rounded-full"
+            src="https://pbs.twimg.com/profile_images/1121328878142853120/e-rpjoJi_bigger.png" alt="" />
+        </div>
+        <div class="ml-3 hidden md:block">
+          <p class="text-sm leading-6 font-medium">
+            筋トレ 太郎
+          </p>
+        </div>
+      </div>
+    </a>
+    <div class="absolute top-11 p-5 shadow-lg text-sm bg-white" v-if="settingDrower">
+      <div class="mb-2 cursor-pointer">設定</div>
+      <div class="cursor-pointer" @click="Logout">ログアウト</div>
     </div>
   </div>
-</a>
 </template>
 
 <script>
+import { authLoginMethods } from '../../../mixins/auth.js'
 export default {
-  
+  mixins: [authLoginMethods],
+  data() {
+    return {
+      settingDrower: false,
+    }
+  },
+  methods: {
+    showDrower() {
+      this.settingDrower = !this.settingDrower
+    },
+    async Logout() {
+      await this.logout()
+      this.$router.push('/login')
+    }
+  },
+  mounted() {
+    const $this = this
+    document.addEventListener("click", function (e) {
+      const target = (e.target).closest(".settingDrawer_wrapper");
+      if (target === null) {
+        $this.settingDrower = false
+      }
+    });
+  }
 }
 </script>
