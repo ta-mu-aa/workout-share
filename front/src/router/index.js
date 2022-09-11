d2ca86cc7ab8f05377c2859c62586e2f049b7fae
ta-mu-aa <tamtamtamtaaaf@gmail.com>
2022-09-11 11:32:49 +0900

    ログイン状態を維持できるように実装

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "../components/pages/HomePage.vue";
 import LoginPage from "../components/pages/LoginPage.vue";
 import SignUpPage from "../components/pages/SignUpPage.vue";
+import axios from "axios";
+import { authLoginMethods } from '../../mixins/auth.js'
 
 const routes = [
   {
@@ -26,4 +28,15 @@ const router = createRouter({
   routes,
 });
 
-export default router;
+router.beforeResolve(async (to) => {
+  if (to.fullPath === '/login' || to.fullPath === '/signup') {
+    return
+  }
+    await axios.post(
+      '/auth_token/refresh',
+      {},
+      { validateStatus: status => authLoginMethods.methods.resolveUnauthorized(status) }
+      ).then(response => authLoginMethods.methods.login(response.data))
+  })
+
+export default router
