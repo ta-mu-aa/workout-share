86c824215371dba8fd104c8030e75ebdaee74641
ta-mu-aa <tamtamtamtaaaf@gmail.com>
2022-09-27 00:10:01 +0900

    認証状態に伴ってユーザーをリダイレクトする

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -4,6 +4,7 @@ import LoginPage from "../components/pages/LoginPage.vue";
 import SignUpPage from "../components/pages/SignUpPage.vue";
 import silent_refresh  from '../../plugins/silent-refresh-token.js'
 import { authLoginMethods } from '../../mixins/auth.js'
+// import { store } from '../../store/index.js'
 import axios from 'axios'
 
 const routes = [
@@ -30,8 +31,15 @@ const router = createRouter({
 });
 
 const auth = authLoginMethods.methods
-router.beforeEach(async () => {
-
+router.beforeEach(async (to,from) => {
+  // ログアウト時の画面遷移では何もしない
+  if (from.path !== '/' && to.path === '/login') {
+    return
+  }
+  // 認証が失敗した際の画面遷移では何もしない
+  if (to.query.auth) {
+    return
+  }
   // ログインしているユーザー情報がセットされていなければ取得する
   if (!auth.isExistUser()) {
     await axios.post(
@@ -44,6 +52,24 @@ router.beforeEach(async () => {
 
   // 画面遷移時にトークンの有効期限が切れていないか判断する
   await silent_refresh()
+
+  // ログインしている場合
+  if (to.path === '/login' || to.path === '/signup') {
+    if (auth.loggedIn()) {
+      console.log('ログイン済みユーザーです')
+      router.push('/home')
+    }
+  }
+  else {
+    // ログインしていない場合
+    if (!auth.loggedIn()) {       
+      // ユーザー以外の値が存在する可能性があるので全てを削除する
+      await auth.logout()
+
+      console.log('まずはログインしてください')
+      router.push({ path:'login', query:{auth:'uncertified'}})
+    }
+  }
 })
 
 export default router
