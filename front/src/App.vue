4a786dbba508c04d338333fc4e7462139bc4bfd8
ta-mu-aa <tamtamtamtaaaf@gmail.com>
2022-10-01 11:48:58 +0900

    グローバルトースター出力から自動解除まで

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,8 +1,25 @@
-<script setup>
+<script>
+  import Toaster from './components/AppToaster.vue' 
+export default {
+  components: {
+    Toaster
+  },
+  methods: {
+    test() {
+      const message = 'test'
+      const color = 'bg-red-500'
+      this.$store.dispatch('getToast', {message, color})
+    }
+  }    
+
+}
 </script>
 
 <template>
+  <button @click = "test">test</button>
+  <Toaster />
   <router-view></router-view>
+
 </template>
 
 <style scoped>
