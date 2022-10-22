8afdb5066941c7f28149d1af32c9a59ac0da37ba
ta-mu-aa <tamtamtamtaaaf@gmail.com>
2022-10-22 11:34:08 +0900

    投稿用のバリデーションを追加し、投稿時エラーの出力まで

--- api/app/controllers/posts_controller.rb
+++ api/app/controllers/posts_controller.rb
@@ -1,17 +1,28 @@
 class PostsController < ApplicationController
 
   def create
-    @post = Post.new(post_params)
-    @post.save
+    post = Post.new(post_params)
+    if post.save
+      render json: post
+    else
+      if post.errors === ["is too long (maximum is 400 characters)"]
+        render json: { message: '400字以内で入力してください', data: post.errors["body"]}, status:400
+      elsif post.errors === ["can't be blank"]
+        render json: { message: '文字を入力してください', data: post.errors["body"] }, status:400    
+      else 
+        render json: { message: '投稿元のユーザーが見つかりませんでした', data: post.errors }, status: 404
+      end
+    end
   end
 
   def index
-    @post = Post.all
-    render json: @post
+    post_all = Post.all
+    render json: post_all
   end
 
   def show
-    @post = Post.find(params[:id])
+    post_unique = Post.find(params[:id])
+    render json: post_unique
   end
 
   private
--- api/app/models/post.rb
+++ api/app/models/post.rb
@@ -1,4 +1,16 @@
 class Post < ApplicationRecord
   # アソシエーション
   belongs_to :user
-end
+
+  validates :body, presence: true,      
+                      length: { maximum: 400 }
+  validates :user_id, presence:true
+  validate :confirm_user_id
+   
+# ユーザーIDが登録されているユーザーのものか確認
+  def confirm_user_id
+    unless User.find_by(id: user_id)
+      errors.add(:user_id, "ユーザーが存在しません")
+    end
+  end
+ end
