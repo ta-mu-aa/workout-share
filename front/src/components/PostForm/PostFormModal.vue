7e9c8a0659dbac3c23203f6564870c2ac7135ffd
ta-mu-aa <tamtamtamtaaaf@gmail.com>
2022-11-02 22:04:52 +0900

    投稿時の処理と例外時の処理を追加

--- front/src/components/PostForm/PostFormModal.vue
+++ front/src/components/PostForm/PostFormModal.vue
@@ -2,9 +2,20 @@
   <div v-if="isVisible">
     <div id="modal-base" class="fixed -inset-0 bg-black bg-opacity-5 flex flex-col justify-center" style=" background-color: rgba(0,0,0,.5);" ></div>
     <div class="fixed top-1/2 left-1/2 bg-white flex flex-col justify-center w-3/5 h-auto rounded-xl p-8 pb-0 -translate-y-2/4 -translate-x-2/4">
-      <textarea class="caret-slate-500 border-b resize-none outline-none" placeholder="投稿内容を入力" name="" cols="20" rows="10" maxlength="400"></textarea>
-      <div class="text-right py-4">
-        <span class="py-2 px-5 bg-blue-400 text-white rounded-full text-sm">投稿</span>  
+      <textarea 
+      v-model="postContent.body" 
+      class="caret-slate-500 border-b resize-none outline-none" 
+      placeholder="投稿内容を入力" cols="20" rows="10"
+      >
+    </textarea>
+      <div class="text-right py-3">
+        <button @click="submitPostContent" 
+        :disabled="buttonDisabled" 
+        class="py-2 px-6 text-white rounded-full text-sm"
+        :class="{ 'bg-blue-300': buttonDisabled == true, 'bg-blue-400': buttonDisabled == false }"
+        >
+        投稿
+      </button>  
       </div>
     </div>
   </div>
@@ -12,10 +23,45 @@
 
 <script>
 export default {
+  data() {
+    return {
+      postContent: {
+        body: '',
+        user_id: this.$store.getters.current_user.id
+      },
+      buttonDisabled: false
+    }
+  },
+  methods: {
+    async submitPostContent() {
+      await this.axios.post('/posts', this.postContent)
+        .then( response => this.postContentSuccess(response))
+        .catch( error => this.postContentError(error))
+      },
+      postContentSuccess() {
+        this.postContent.body = ''
+        this.$emit("closeFormModal", false)
+      },
+      postContentError(error) {
+        const message = error.response.data.message
+        this.$store.dispatch('getToast', { message })
+      }
+  },
   computed: {
     isVisible() {
       return this.$store.getters.postFormModal
-    } 
+    }, 
+  },
+  updated() {
+    if (this.postContent.body === '') {
+      this.buttonDisabled = true
+    }
+    else if (this.postContent.body.length > 400) {
+      this.buttonDisabled = true
+    }
+    else {
+      this.buttonDisabled = false
+    }
   }
 }
 </script>
