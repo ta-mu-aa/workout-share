e80c8a8c38f284ce43466e871826b6d469631887
ta-mu-aa <tamtamtamtaaaf@gmail.com>
2022-11-05 15:49:06 +0900

    編集と削除を行うためのドロワーを実装

--- front/src/components/PostList/Post.vue
+++ front/src/components/PostList/Post.vue
@@ -1,33 +1,38 @@
 <template>
-<div v-for="post in showPostList" :key="post.id">
-  <div class="flex flex-shrink-0 p-4 pb-0">
+<div>
+  <div v-for="post in showPostList" :key="post.id" :id=post.id>
+    <div class="relative flex flex-shrink-0 p-4 pb-0">
     <a href="#" class="flex-shrink-0 group block">
       <div class="flex items-center">
         <PostUserinfo />
-        <div class="ml-3">
-          <span class="text-sm leading-5 ml-2 font-medium text-gray-400">
-            {{ postedTime(post.created_at)}}
-          </span>
-        </div>
       </div>
     </a>
+    <div class="m-auto text-sm leading-5 ml-2 font-medium text-gray-400">
+      {{ postedTime(post.created_at)}}
+    </div>
+    <svg @click="showPostDrawer(post.id); selectePostId(post.id)" class="post-drawer-icon w-8 h-8 p-1 my-auto cursor-pointer rounded-full duration-200 hover:bg-black hover:text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+        d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z">
+      </path>
+    </svg>
+    <PostDrawer :showPostDrawer="postDrawerBoolean" :selectedPostId = "selectedPost" v-if="post.id === selectedPost" />
   </div>
   <div class="pl-16">
     <p class="text-base width-auto font-medium  flex-shrink">
       {{ post.body }}
       <span class="text-blue-400"># リンクが入ります</span>
     </p>
-
     <div class="flex">
       <div class="w-full">
-        <div class="flex justify-end items-center">
-          <PostReply />
-          <PostFavorite />
+          <div class="flex justify-end items-center">
+            <PostReply />
+            <PostFavorite />
+          </div>
         </div>
-      </div>
-    </div>  
+      </div>  
+    </div>
+    <hr class="border-gray-600">
   </div>
-  <hr class="border-gray-600">
 </div>
 </template>
 
@@ -35,23 +40,46 @@
 import PostUserinfo from './PostUserinfo.vue'
 import PostReply from './PostReply.vue'
 import PostFavorite from './PostFavorite.vue'
+import PostDrawer from './PostDrawer.vue'
 export default {
   components: {
     PostUserinfo,
     PostReply,
-    PostFavorite
+    PostFavorite,
+    PostDrawer
+  },
+  data() {
+    return {
+      postDrawerBoolean: false,
+      selectedPost: null
+    }
   },
   methods: {
     postedTime(time) {
       const postedMonth = time.slice(5, 7)
       const postedDate = time.slice(8, 10)
       return postedMonth + '月' + postedDate + '日'
+    },
+    showPostDrawer() {
+      this.postDrawerBoolean = !this.postDrawerBoolean
+    },
+    selectePostId(id) {
+      this.selectedPost = id
     }
   },
   computed: {
     showPostList() {
       return this.$store.getters.postList
     }
+  },
+  mounted() { 
+    const $this = this
+    document.addEventListener("click", function (e) {
+      const target = (e.target).closest(".post-drawer-icon")
+      if (target === null && $this.postDrawerBoolean) {
+        $this.showPostDrawer()
+      }
+    })
   }
 }
 </script>
---
+++ front/src/components/PostList/PostDrawer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="absolute top-14 right-0 min-w-max p-5 shadow-lg text-sm bg-white" v-if="showPostDrawer">
+    <div class="mb-2 cursor-pointer" @click="postUpdate">編集</div>
+    <div class="cursor-pointer" @click="postDelete">削除</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    showPostDrawer: {
+      type: Boolean
+    },
+    selectedPostId: {
+      type: Number
+    }
+  },
+  methods: {
+    postUpdate() {
+      console.log('update')
+    },
+    postDelete() {
+      console.log('delete')
+    }
+  }
+}
+</script>
