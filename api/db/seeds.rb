86d0931cc6bb162aa7472ef1498f45fda75f8297
ta-mu-aa <tamtamtamtaaaf@gmail.com>
2023-01-21 12:31:06 +0900

    ユーザーと投稿のテストデータを自動作成

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |n|
+ User.create!(
+   name: "ユーザー#{n+1}",
+   email: "user#{n+1}@example.com",
+   password: "password00" ,
+   password_confirmation: "password00",
+   user_discription: "ユーザー#{n+1}です。よろしくお願いします。"
+)
+end
+
+s1 = Date.parse("2022/09/01")
+s2 = Date.parse("2023/01/01")
+User.all.each do |user|
+  2.times do |n|
+    s = Random.rand(s1 .. s2)
+    Post.create!(
+      user_id: user.id,
+      body: "#{user.name}の投稿#{n+1}です" + "こんにちは" + 
+      "\n投稿内容が入ります投稿内容が入ります投稿内容が入ります投稿内容が入ります投稿内容が入ります投稿内容が入ります投稿内容が入ります投稿内容が入ります投稿内容が入ります投稿内容が入ります投稿内容が入ります投稿内容が入ります投稿内容が入ります投稿内容が入ります投稿内容が入ります",
+      created_at: s
+    )
+  end
+end
